c716ebe1e7b79dcf5322b575ece0c0f914ee13ab
ZodicSlanser <zodic.slanser68@yahoo.com>
2024-06-19 22:13:27 +0300

    finalized basic unit testing

--- src/cart/specs/cart.controller.spec.ts
+++ src/cart/specs/cart.controller.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CartController } from '../cart.controller';
+import { CartService } from '../cart.service';
 
 describe('CartController', () => {
   let controller: CartController;
+  let service: CartService;
 
   beforeEach(async () => {
+    service = {
+      addToCart: jest.fn().mockResolvedValue({ cartId: 1, productId: 1, quantity: 1 }),
+      getCart: jest.fn(),
+      updateCart: jest.fn(),
+      removeFromCart: jest.fn()
+    } as any;
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CartController],
+      providers: [{ provide: CartService, useValue: service }],
     }).compile();
 
     controller = module.get<CartController>(CartController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should add item to cart', async () => {
+    const updateCartDto = { userId: 1, productId: 1, quantity: 1 };
+
+    expect(await controller.addToCart(updateCartDto)).toEqual({ cartId: 1, productId: 1, quantity: 1 });
+  });
+
+  it('should get cart', async () => {
+    const userId = 1;
+    service.getCart = jest.fn().mockResolvedValue({ userId, items: [] });
+
+    expect(await controller.getCart(userId)).toEqual({ userId, items: [] });
+  });
+
+  it('should update cart', async () => {
+    const updateCartDto = { userId: 1, productId: 1, quantity: 2 };
+    service.updateCart = jest.fn().mockResolvedValue({ cartId: 1, productId: 1, quantity: 2 });
+
+    expect(await controller.updateCart(updateCartDto)).toEqual({ cartId: 1, productId: 1, quantity: 2 });
+  });
+
+  it('should remove item from cart', async () => {
+    const removeItemDto = { userId: 1, productId: 1 };
+    service.removeFromCart = jest.fn().mockResolvedValue({ message: 'Product removed from cart' });
+
+    expect(await controller.removeFromCart(removeItemDto)).toEqual({ message: 'Product removed from cart' });
   });
 });
--- src/cart/specs/cart.service.spec.ts
+++ src/cart/specs/cart.service.spec.ts
@@ -1,18 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CartService } from '../cart.service';
+import { prismaMock, PrismaMockService } from '../../prisma/prisma.service.mock';
+import { PrismaService } from '../../prisma/prisma.service';
+import { UpdateCartDto } from '../DTOs/update-cart.dto';
 
 describe('CartService', () => {
   let service: CartService;
+  let prismaMockService: PrismaMockService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CartService],
+      providers: [
+        CartService,
+        { provide: PrismaService, useValue: prismaMock },
+      ],
     }).compile();
 
     service = module.get<CartService>(CartService);
+    prismaMockService = module.get<PrismaMockService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should add item to cart', async () => {
+    const updateCartDto = { userId: 1, productId: 1, quantity: 1 };
+    prismaMockService.cart.upsert.mockResolvedValue({ cartId: 1, userId: 1 });
+    prismaMockService.cartItem.upsert.mockResolvedValue({ cartId: 1, productId: 1, quantity: 1 });
+
+    expect(await service.addToCart(updateCartDto)).toEqual({ cartId: 1, productId: 1, quantity: 1 });
+  });
+    it('should get cart', async () => {
+    const userId = 1;
+    prismaMockService.cart.findUnique.mockResolvedValue({ userId, items: [] });
+
+    expect(await service.getCart(userId)).toEqual({ userId, items: [] });
+  });
+
+  it('should update cart', async () => {
+    const updateCartDto = { userId: 1, productId: 1, quantity: 2 };
+    prismaMockService.cart.findUnique.mockResolvedValue({ cartId: 1, userId: 1 });
+    prismaMockService.cartItem.update.mockResolvedValue({ cartId: 1, productId: 1, quantity: 2 });
+
+    expect(await service.updateCart(updateCartDto)).toEqual({ cartId: 1, productId: 1, quantity: 2 });
+  });
+
+  it('should remove item from cart', async () => {
+    const removeItemDto = { userId: 1, productId: 1 };
+    prismaMockService.cart.findUnique.mockResolvedValue({ cartId: 1, userId: 1 });
+    prismaMockService.cartItem.delete.mockResolvedValue({});
+
+    expect(await service.removeFromCart(removeItemDto)).toEqual({ message: 'Product removed from cart' });
+  });
+
+  it('should remove item from cart when quantity less than 1', async () => {
+    const UpdateCartDto:UpdateCartDto = { userId: 1, productId: 1, quantity: 0 };
+    prismaMockService.cart.findUnique.mockResolvedValue({ cartId: 1, userId: 1 });
+    prismaMockService.cartItem.delete.mockResolvedValue({});
+
+    expect(await service.updateCart(UpdateCartDto)).toEqual({ message: 'Product removed from cart' });
   });
 });
